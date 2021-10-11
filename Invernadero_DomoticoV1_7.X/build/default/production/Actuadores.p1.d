5b05656648d50234ae8c48649a5abe912a76b465
jabella1 <83263980+jabella1@users.noreply.github.com>
2021-10-11 17:10:59 -0500

    Delete Invernadero_DomoticoV1_7.X directory

--- Invernadero_DomoticoV1_7.X/build/default/production/Actuadores.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/Actuadores.p1:  \
-Actuadores.c  \
-Actuadores.h  \
-pinout.h  \
-Config.h 
--- Invernadero_DomoticoV1_7.X/build/default/production/Main_Invernadero.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/production/Main_Invernadero.p1:  \
-Main_Invernadero.c  \
-Config.h  \
-pinout.h  \
-LCD.h  \
-keypad.h  \
-DHT11.h  \
-Actuadores.h  \
-Variables.h 
