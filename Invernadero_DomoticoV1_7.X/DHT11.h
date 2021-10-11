82198c28b1aed644849f79e5047de558b5cfd156
jabella1 <83263980+jabella1@users.noreply.github.com>
2021-10-11 15:02:03 -0500

    Add files via upload
    Version 1.7

---
+++ Invernadero_DomoticoV1_7.X/DHT11.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   DHT11.h
+ * Author: Servitech D&M
+ *
+ * Created on 7 de octubre de 2021, 10:52 PM
+ */
+
+#include <stdbool.h>
+
+#ifndef DHT11_H
+#define	DHT11_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void DHT11_Start();
+void DHT11_CheckResponse();
+char DHT11_ReadData();
+
+
+void DatosSensorTH(char Datos[]);
+void Comparar_T_H_Adecuados(char *TempActual,char *HumActual,int TempDigitada,
+        int HumDigitada,int MargenErrorT,int MargenErrorH);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DHT11_H */
+
---
+++ Invernadero_DomoticoV1_7.X/build/default/production/Actuadores.p1.d
@@ -0,0 +1,5 @@
+build/default/production/Actuadores.p1:  \
+Actuadores.c  \
+Actuadores.h  \
+pinout.h  \
+Config.h 
