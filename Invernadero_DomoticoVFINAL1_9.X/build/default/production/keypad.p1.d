464fb8265ec47440b03abfabf531d7c55547c6a5
jabella1 <83263980+jabella1@users.noreply.github.com>
2021-10-12 10:23:49 -0500

    Delete Invernadero_DomoticoVFINAL1_9.X directory

--- Invernadero_DomoticoVFINAL1_9.X/build/default/production/keypad.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/keypad.p1:  \
-keypad.c  \
-keypad.h  \
-Config.h  \
-pinout.h 
--- Invernadero_DomoticoVFINAL1_9.X/dist/default/production/doprnt.d
+++
@@ -1,2 +0,0 @@
-dist/default/production\doprnt.p1: \
-  D:\XC8\ compilador\ para\ el\ MPLAB\pic\sources\c99\common\doprnt.c
