22c9d8e661d40e63c677320ed2d44f7e180e3fae
jabella1 <83263980+jabella1@users.noreply.github.com>
2021-10-12 10:25:33 -0500

    Add files via upload
    Versión 1.9

---
+++ Invernadero_DomoticoV1_9.X/keypad.c
@@ -0,0 +1,157 @@
+#include <xc.h>
+#include "keypad.h"
+
+
+void keypad_init(void){
+   
+    PTECLADO &= ~(1<<PB0) & ~(1<<PB1) & ~(1<<PB2) & ~(1<<PB3);   // FILAS    - SALIDA
+    PTECLADO |= (1<<PB4) | (1<<PB5) | (1<<PB6) | (1<<PB7);       // COLUMNAS - ENTRADA
+    INTCON2bits.RBPU = 0;   //   PORTB pull-ups are enabled by individual port latch values 
+}
+
+unsigned char col_loc,rowloc,temp_col;
+
+unsigned char keypad[4][3]= {'1','2','3',
+                             '4','5','6',
+                             '7','8','9',
+                             '*','0','#'};
+
+
+// unsigned char keyfind(). Retorna la tecla que se presione en el teclado como un char sin signo.
+unsigned char keyfind()
+{      
+    
+         Direction_Port = 0xf0;  /* PORTD.0-PORTD.3 as a Output Port and PORTD.4-PORTD.7 as a Input Port*/
+         write_port = 0xf0;  /* Make lower nibble as low(Gnd) and Higher nibble as High(Vcc) */
+      do
+      {
+        do
+        {
+          
+            col_loc = read_port & 0xf0;  /* mask port with f0 and copy it to col_loc variable */   
+        
+        }while(col_loc!=0xf0);  /* Check initially at the start there is any key pressed*/ 
+         col_loc = read_port & 0xf0;  /* mask port with f0 and copy it to col_loc variable */  
+      }while(col_loc!=0xf0);
+      
+      __delay_ms(30);
+        write_port = 0xf0;  /* Make lower nibble as low(Gnd) and Higher nibble as High(Vcc) */
+        do
+        { do
+            
+            {
+                col_loc = read_port & 0xf0;
+            }while(col_loc==0xf0);  /* Wait for key press */
+         col_loc = read_port & 0xf0;
+        }while(col_loc==0xf0);  /* Wait for key press */
+               
+        __delay_ms(30);
+         
+        col_loc = read_port & 0xf0;
+               
+   
+    
+    while(1)
+    {
+        write_port  = 0xfe;  /* make Row0(D0) Gnd and keep other row(D1-D3) high */
+        col_loc = read_port & 0xf0;         /* Read Status of PORT for finding Row */
+        temp_col=col_loc;
+        if(col_loc!=0xf0)
+        {
+            rowloc=0;  /* If condition satisfied get Row no. of key pressed */
+            while(temp_col!=0xf0)  /* Monitor the status of Port and Wait for key to release */
+            {
+                temp_col = read_port & 0xf0;  /* Read Status of PORT for checking key release or not */    
+            }
+            break;
+        }
+        
+        write_port = 0xfd;  /* make Row1(D1) Gnd and keep other row(D0-D2-D3) high */
+        col_loc = read_port & 0xf0;  /* Read Status of PORT for finding Row */
+        temp_col=col_loc;
+        if(col_loc!=0xf0)
+        {
+            rowloc=1;  /* If condition satisfied get Row no. of key pressed*/
+            while(temp_col!=0xf0)  /* Monitor the status of Port and Wait for key to release */
+            {
+                temp_col = read_port & 0xf0;  /* Read Status of PORT for checking key release or not */
+            }
+            break;
+        }
+        
+        write_port = 0xfb;  /* make Row0(D2) Gnd and keep other row(D0-D1-D3) high */
+        col_loc = read_port & 0xf0;  /* Read Status of PORT for finding Row*/
+        temp_col=col_loc;
+        if(col_loc!=0xf0)
+        {
+            rowloc=2;  /* If condition satisfied get Row no. of key pressed */
+            while(temp_col!=0xf0)  /* Wait for key to release */
+            {
+                temp_col = read_port & 0xf0;  /* Read Status of PORT for checking key release or not */
+            }
+            break;
+        }
+        
+        write_port = 0xf7;  /* make Row0(D3) Gnd and keep other row(D0-D2) high */
+        col_loc = read_port & 0xf0;  /* Read Status of PORT for finding Row */
+        temp_col=col_loc;
+        if(col_loc!=0xf0)
+        {
+            rowloc=3;  /* If condition satisfied get Row no. of key pressed */
+            while(temp_col!=0xf0)  /* Wait for key to release */
+            {
+                temp_col = read_port & 0xf0;  /* Read Status of PORT for checking key release or not */
+            }
+            break;
+        }
+        
+    }
+
+    
+    while(1)
+    {
+        
+        if(col_loc==0xe0)
+        {
+             return keypad[rowloc][0];  /* Return key pressed value to calling function */            
+        }
+        else 
+            if(col_loc==0xd0)
+        {
+            return keypad[rowloc][1];  /* Return key pressed value to calling function */   
+        }
+        else
+            if(col_loc==0xb0)
+        {
+            return keypad[rowloc][2];  /* Return key pressed value to calling function */
+        }
+    }
+    
+   __delay_ms(30);     
+ 
+}
+
+
+//Recibe como parametro un vector de tipo unsigned char el cual ya estará lleno
+//Lo que hace es convertir los datos que tiene ese vector de char a entero y despues retornarlo.
+
+int DatoDigitadoAEntero(unsigned char prmTemperatura[]){
+    int Decena = 0;
+    int Unidad = 0;
+    
+    int Resultado=0;
+    
+    Decena = prmTemperatura[0]-'0';
+    Unidad = prmTemperatura[1]-'0';
+    
+    Decena = Decena * 10;
+    Resultado = Decena + Unidad;
+    
+    return Resultado;
+}
+
+//conecta el puerto b a 5vcc a traves de una resistencia interna
+//de esta manera se puede usar el teclado matriarcal.
+void initTeclado(){
+    RBPU=0; /* activate pull-up resistor */
+}
