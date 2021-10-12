6f70207dd977a50df3eb6bb647221f2b22dff58e
jabella1 <83263980+jabella1@users.noreply.github.com>
2021-10-11 22:16:36 -0500

    Add files via upload
    Versión 1.8 FINAL.

---
+++ Invernadero_DomoticoVFINAL1_8.X/Actuadores.h
@@ -0,0 +1,38 @@
+/* 
+ * File:   Actuadores.h
+ * Author: Servitech D&M
+ *
+ * Created on 10 de octubre de 2021, 08:43 PM
+ */
+
+#ifndef ACTUADORES_H
+#define	ACTUADORES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#include <stdbool.h>
+    
+    
+void initActuadores();    
+void ApagarAspersores();
+void ApagarVentiladores();
+void ApagarLeds();
+void ApagarCalefactor();
+
+void PrenderAspersores();
+void PrenderVentiladores();
+void PrenderCalefactor();
+
+void PrenderLed(bool TemperaturaAdecuada,bool HumedadAdecuada);
+void ControlTemperaturaVentiladorCalefactor(char *TempActual,int TempDigitada,int MargenErrorT);
+void ControlHumedadAspersor(char *HumedadActual,int HumedadDigitada,int MargenErrorH);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ACTUADORES_H */
+
